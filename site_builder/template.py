75e9c654d15e9cf73d38a257cf171d70a6ffa6d7
KonstantinSh <Kverde@yandex.ru>
2022-10-16 23:58:41 +0500

    Добавлена возможность комбинирования шаблонов

--- site_builder/template.py
+++ site_builder/template.py
@@ -20,17 +20,24 @@ TEMPLEATES = {
 }
 
 
-def field_1(match):
-    template_name = match.group(1)
-    content = match.group(2)
-
+def template_1(template_name: str, content: str):
     if template_name not in TEMPLEATES:
         raise TemplateError(f'Template {template_name} not found')
 
     return TEMPLEATES[template_name](content)
 
 
+def process_template(match):
+    combiend_template = match.group(1)
+    content = match.group(2)
+
+    for template_name in reversed(combiend_template.split('+')):
+        content = template_1(template_name, content)
+
+    return content
+
+
 def prepare(text: str) -> str:
-    result = re.sub(r"{{([\w]*)\|(.*?)}}", field_1,
+    result = re.sub(r"{{([\w+]*)\|(.*?)}}", process_template,
                     text, flags=re.MULTILINE | re.DOTALL)
     return result
