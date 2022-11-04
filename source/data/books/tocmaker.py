07d9d4fdf153b35601d6670974e09c9cfb01fd3a
KonstantinSh <Kverde@yandex.ru>
2022-11-04 21:45:16 +0500

    add tocmaker

---
+++ source/data/books/tocmaker.py
@@ -0,0 +1,19 @@
+import os
+
+ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
+
+FILENAME = os.path.join(ROOT_PATH, 'VilenkinRasskazyMnozhestvah2005.toc')
+
+with open(FILENAME, encoding='utf-8') as file:
+    text = file.read()
+
+lines = text.split('\n')
+
+for i, line in enumerate(lines):
+    id = f'{i + 1:0>3}'
+    if not line.startswith(id):
+        lines[i] = id + ' ' + line
+
+text = '\n'.join(lines)
+file = open(FILENAME, 'w', encoding='utf-8')
+file.write(text)
