a533f76719cb0c65f9c6bb85a10df371c7405fb3
KonstantinSh <Kverde@yandex.ru>
2023-02-05 20:41:48 +0500

    upd

--- source/data/toc/_tocmaker.py
+++ source/data/toc/_tocmaker.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
 
@@ -12,8 +13,14 @@ lines = text.split('\n')
 
 for i, line in enumerate(lines):
     id = f'{i + 1:0>3}'
-    if not line.startswith(id):
-        lines[i] = id + ' ' + line
+
+    if line.startswith(id):
+        continue
+
+    if re.match(r'^\d\d\d.*', line):
+        line = line[3:]
+
+    lines[i] = id + ' ' + line
 
 text = '\n'.join(lines)
 file = open(FILENAME, 'w', encoding='utf-8')
